4e30a6608411e7f38fe89f848c8f5e9ff669613a
John Mayou <johnm.ceo@gmail.com>
2023-01-27 19:57:29 -0600

    solved for expressions with multiple operators, checking validity of input and parenthesis still needs to be done

--- server/server.js
+++ server/server.js
@@ -17,8 +17,34 @@ let expressionsList = [
 ];
 
 app.post("/expressions", (req, res) => {
-	const expressionParts = req.body.parts;
-	const answer = Number(expressionParts[0]) + Number(expressionParts[2]);
+	let expressionParts = req.body.parts;
+	expressionParts = expressionParts.map((elem) => {
+		let numberRegex = /[0-9]+(\.[0-9]+)?/;
+		return numberRegex.test(elem) ? Number(elem) : elem;
+	}); // changing string numbers to numbers
+
+	let answer;
+	let copyExpressionParts = [...expressionParts];
+
+	// finding answer
+	while (copyExpressionParts.length > 1) {
+		let indexToSlice =
+			findHighestOrderOfOperations(copyExpressionParts) - 1;
+		let sliceToEvaluate = copyExpressionParts.slice(
+			indexToSlice,
+			indexToSlice + 3
+		);
+		copyExpressionParts.splice(
+			indexToSlice,
+			3,
+			operateOnTwoArgs(sliceToEvaluate)
+		);
+	}
+	// when done evaluating parts
+	answer = copyExpressionParts[0];
+
+	console.log("Parts: ", expressionParts);
+	console.log("Answer", answer);
 
 	const newExpressionObj = {
 		expressionParts,
@@ -33,6 +59,40 @@ app.get("/expressions", (req, res) => {
 	res.send(expressionsList);
 	res.sendStatus(200);
 });
+
+// Helper functions
+function findHighestOrderOfOperations(array) {
+	if (array.indexOf("*") !== -1) {
+		return array.indexOf("*");
+	} else if (array.indexOf("/") !== -1) {
+		return array.indexOf("/");
+	} else if (array.indexOf("+") !== -1) {
+		return array.indexOf("+");
+	} else if (array.indexOf("-") !== -1) {
+		return array.indexOf("-");
+	} else {
+		console.log("Error finding order of operations");
+	}
+}
+
+function operateOnTwoArgs(array) {
+	const arg1 = array[0];
+	const operator = array[1];
+	const arg2 = array[2];
+
+	switch (operator) {
+		case "*":
+			return arg1 * arg2;
+		case "/":
+			return arg1 / arg2;
+		case "+":
+			return arg1 + arg2;
+		case "-":
+			return arg1 - arg2;
+		default:
+			console.log("Error operating on two args");
+	}
+}
 
 // Listen
 const PORT = 5000;
